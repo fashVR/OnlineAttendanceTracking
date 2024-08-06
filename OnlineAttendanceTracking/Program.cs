839de729985f53167e286a74b945a44196665cd5
fashVR <myroy2005@gmail.com>
2024-08-06 15:52:58 +0300

    new changes to feature branch, attempted fixing branch structure.

--- OnlineAttendanceTracking/Program.cs
+++ OnlineAttendanceTracking/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello student! Insert your name: ");
+        string studentName = Console.ReadLine() ?? string.Empty;
+
+        Console.WriteLine(string.Format("hello {0}!", studentName));
+    }
+}
